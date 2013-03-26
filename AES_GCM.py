70809476fd7f7c5a6a990f96b61591f022e6e7d0
Bo Zhu <github@bozhu.me>
2013-03-25 23:07:44 -0400

    finished a simple version of encryption, but havent tested

--- AES_GCM.py
+++ AES_GCM.py
@@ -5,6 +5,22 @@ from Crypto.Util import Counter
 from Crypto.Util.number import long_to_bytes, bytes_to_long
 
 
+# GF(128) defined by 1 + a + a^2 + a^7 + a^128
+def gf128_mul(x, y):
+    assert x < 1 << 128
+    assert y < 1 << 128
+    res = 0
+    for i in range(128):
+        res ^= x * (y & 1)
+        y >>= 1
+
+        rmd = (x >> 127) * 0b10000111
+        x = (x % (1 << 127)) << 1
+        x ^= rmd
+    assert res < 1 << 128
+    return res
+
+
 # Galois/Counter Mode with AES-128 and 96-bit IV
 class AES_GCM:
     def __init__(self, master_key):
@@ -16,8 +32,21 @@ class AES_GCM:
         self.__master_key = long_to_bytes(master_key, 16)
         self.__aes_ecb = AES.new(self.__master_key, AES.MODE_ECB)
         self.__auth_key = bytes_to_long(self.__aes_ecb.encrypt(b'\x00' * 16))
-        # TODO precompute the table for multiplication in finite field
-        self.__times_H = _gen_table(self.__auth_key)
+
+        # precompute the table for multiplication in finite field
+        table = []
+        for i in range(16):
+            row = []
+            for j in range(256):
+                row.append(gf128_mul(self.__auth_key, j << (8 * i)))
+            table.append(tuple(row))
+        self.__pre_table = tuple(table)
+
+    def times_auth_key(self, val):
+        res = 0
+        for i in range(16):
+            res ^= self.__pre_table[i][val & 0xFF]
+            val >>= 8
 
     def encrypt(self, init_value, plaintext, auth_data=b''):
         assert init_value < (1 << 96)
@@ -41,21 +70,21 @@ class AES_GCM:
         if len_auth_data > 0:
             pass
         if len_plaintext > 0:
-            # TODO
-            pass
+            for i in range(len_plaintext // 16):
+                auth_tag ^= bytes_to_long(ciphertext[i * 16: (i + 1) * 16])
+                auth_tag = self.__times_auth_key(auth_tag)
+        auth_tag ^= self.__aes_ecb.encrypt(
+                long_to_bytes(init_value << 32 | 1, 16))
 
         # unpad ciphertext
+        assert len(ciphertext) == len_plaintext
+        assert auth_tag < (1 << 128)
         return ciphertext, auth_tag
 
     def decrypt(self, init_value, ciphertext, auth_data=b''):
-        # TODO
         assert init_value < (1 << 96)
-
-
-def _gen_table():
-    # TODO
-    pass
+        # TODO
 
 
 if __name__ == '__main__':
-    pass
+    print hex(gf128_mul(32, 1213))
